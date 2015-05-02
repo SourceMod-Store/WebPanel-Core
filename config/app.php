4d71ca88a7c8c697627c6df91dd1c085c611c4f5
Werner Maisl <werner@myhomenet.at>
2015-05-02 19:18:20 +0200

    Changed Filesystem from S3 to local Fixed an issue with artisan

--- config/app.php
+++ config/app.php
@@ -135,7 +135,6 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-		'Entrust' => 'Zizaco\Entrust\EntrustFacade',
 
 		/*
 		 * Application Service Providers...
@@ -194,6 +193,7 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Entrust'    => 'Zizaco\Entrust\EntrustFacade',
 
 	],
 
