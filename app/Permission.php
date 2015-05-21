6c4311a07513826d782c2891ffe0695e953762b4
Werner Maisl <werner@myhomenet.at>
2015-05-21 02:01:50 +0200

    Permissions and Roles can now be mass-assigned

--- app/Permission.php
+++ app/Permission.php
@@ -5,6 +5,8 @@ use Zizaco\Entrust\EntrustPermission;
 class Permission extends EntrustPermission
 {
 
+    protected $fillable = ['name', 'display_name', 'description'];
+
     public function getRoleListAttribute()
     {
         return $this->roles->lists('id');
--- app/Role.php
+++ app/Role.php
@@ -5,6 +5,8 @@ use Zizaco\Entrust\EntrustRole;
 class Role extends EntrustRole
 {
 
+    protected $fillable = ['name', 'display_name', 'description'];
+
     public function getPermissionListAttribute()
     {
         return $this->perms->lists('id');
