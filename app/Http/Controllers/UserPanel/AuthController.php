6f89db3b7f3a44bb077bf34ac1ab9ee9f81ee220
Werner Maisl <werner@myhomenet.at>
2015-09-27 17:36:41 +0200

    Added some output to the steamlogin for testing

--- app/Http/Controllers/UserPanel/AuthController.php
+++ app/Http/Controllers/UserPanel/AuthController.php
@@ -90,7 +90,39 @@ class AuthController extends Controller
      */
     public function steamlogin(Request $request)
     {
-        return SteamLogin::validate();
+        //Handle the return and try to validate it
+        try
+        {
+            $community = SteamLogin::validate();
+        }
+        catch(Exception $e)
+        { //If not possible show 401 error
+            abort(401);
+        }
+
+        echo "<p> Community:".$community."</p>";
+
+        //Get the auth string for the steamid64
+        $steam = $this->communityid_to_steam($community);
+        $auth = $this->steamid_to_auth($steam);
+
+        echo "<p> Steam:".$steam."</p>";
+        echo "<p> Auth:".$auth."</p>";
+
+        //Get the userid for the auth string
+        $user = StoreUser::where("auth",$auth)->first();
+        var_dump($user);
+
+        //Check if the user exists / FirstOrFail the query
+
+        //Set the session vars
+//        $request->session()->put('store_user_authmethod','steam_openid');
+//        $request->session()->put('store_user_id',$user->id);
+//        $request->session()->put('store_user_name',$user->name);
+//        $request->session()->put('store_user_auth',$user->auth);
+
+        //Redirect the user to the User Dashboard
+        //return redirect()->route('userpanel.dashboard');
     }
 
     /**
@@ -104,5 +136,62 @@ class AuthController extends Controller
         $request->session()->forget("store_user_id");
         echo "logged out";
         //TODO: Add a proper view
+    }
+
+
+    //TODO: Move that to its own library
+    //TODO: Add Tests
+    private function steamid_to_auth($steamid)
+    {
+        if (substr_count($steamid, ":") != 2)
+            return 0;
+        //from https://forums.alliedmods.net/showpost.php?p=1890083&postcount=234
+        $toks = explode(":", $steamid);
+        $odd = (int) $toks[1];
+        $halfAID = (int) $toks[2];
+        return ($halfAID * 2) + $odd;
+    }
+    private function steamid_to_community($steamid)
+    {
+        $parts = explode(':', str_replace('STEAM_', '', $steamid));
+        $result = bcadd(bcadd('76561197960265728', $parts['1']), bcmul($parts['2'], '2'));
+        $remove = strpos($result, ".");
+        if ($remove != false)
+        {
+            $result = substr($result, 0, strpos($result, "."));
+        }
+        return $result;
+    }
+    private function communityid_to_steam($i64friendID)
+    {
+        $tmpfriendID = $i64friendID;
+        $iServer = "1";
+        if (bcmod($i64friendID, "2") == "0")
+        {
+            $iServer = "0";
+        }
+        $tmpfriendID = bcsub($tmpfriendID, $iServer);
+        if (bccomp("76561197960265728", $tmpfriendID) == -1)
+        {
+            $tmpfriendID = bcsub($tmpfriendID, "76561197960265728");
+        }
+        $tmpfriendID = bcdiv($tmpfriendID, "2");
+        return ("STEAM_0:" . $iServer . ":" . $tmpfriendID);
+    }
+    private function auth_to_steamid($authid)
+    {
+        $steam = array();
+        $steam[0] = "STEAM_0";
+        if ($authid % 2 == 0)
+        {
+            $steam[1] = 0;
+        }
+        else
+        {
+            $steam[1] = 1;
+            $authid -= 1;
+        }
+        $steam[2] = $authid / 2;
+        return $steam[0] . ":" . $steam[1] . ":" . $steam[2];
     }
 }
