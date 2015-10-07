91c2180cb9a6ddf9cb25e7807f9cd85b8273b692
Werner Maisl <werner@myhomenet.at>
2015-10-08 00:21:54 +0200

    Increased the size of the MotD window

--- resources/views/templates/adminlte205/userpanel/csgoredirector.blade.php
+++ resources/views/templates/adminlte205/userpanel/csgoredirector.blade.php
@@ -2,10 +2,10 @@
 <head>
     <script type="text/javascript">
         window.onload = function(){
-            var popup=window.open("{{route($namedroute)}}","{{$title}}","height=60,width=393");
+            var popup=window.open("{{route($namedroute)}}","{{$title}}","height=720,width=1280");
         };
     </script>
 </head>
 <body>
 </body>
-</html>  
+</html>
