57a99d211b841c790f38745806699f0d2e3bef08
Werner Maisl <werner@myhomenet.at>
2015-10-24 01:15:28 +0200

    Removed the subscribe and unsubscribe buttons from the loadout overview

--- resources/views/templates/adminlte205/userpanel/loadouts/_overviewactions.blade.php
+++ resources/views/templates/adminlte205/userpanel/loadouts/_overviewactions.blade.php
@@ -13,16 +13,16 @@
     {{--</div>--}}
     @endif
     <!-- TODO: Check if the User is already subscribed / unsubscribed -->
-    <div style="float: right">
-        {!! Form::open(['method' => 'get', 'url' => route('userpanel.loadouts.unsubscribe',['loadoutid'=>$loadout->id])]) !!}
-        {!! Form::submit('Un-Subscribe',['class' => 'btn btn-warning']) !!}
-        {!! Form::close() !!}
-    </div>
-    <div style="float: right">
-        {!! Form::open(['method' => 'get', 'url' => route('userpanel.loadouts.subscribe',['loadoutid'=>$loadout->id])]) !!}
-        {!! Form::submit('Subscribe',['class' => 'btn btn-success']) !!}
-        {!! Form::close() !!}
-    </div>
+    {{--<div style="float: right">--}}
+        {{--{!! Form::open(['method' => 'get', 'url' => route('userpanel.loadouts.unsubscribe',['loadoutid'=>$loadout->id])]) !!}--}}
+        {{--{!! Form::submit('Un-Subscribe',['class' => 'btn btn-warning']) !!}--}}
+        {{--{!! Form::close() !!}--}}
+    {{--</div>--}}
+    {{--<div style="float: right">--}}
+        {{--{!! Form::open(['method' => 'get', 'url' => route('userpanel.loadouts.subscribe',['loadoutid'=>$loadout->id])]) !!}--}}
+        {{--{!! Form::submit('Subscribe',['class' => 'btn btn-success']) !!}--}}
+        {{--{!! Form::close() !!}--}}
+    {{--</div>--}}
     @if($loadout->owner_id == $user_id)
         <div style="float: right">
             {!! Form::open(['method' => 'get', 'url' => route('userpanel.loadouts.edit',['loadoutid'=>$loadout->id])]) !!}
@@ -38,7 +38,7 @@
     @endif
     <div style="float: right">
         {!! Form::open(['method' => 'post', 'url' => route('userpanel.loadouts.select',['loadoutid'=>$loadout->id])]) !!}
-        {!! Form::submit('Make Primary',['class' => 'btn btn-success']) !!}
+        {!! Form::submit('Equip loadout',['class' => 'btn btn-success']) !!}
         {!! Form::close() !!}
     </div>
 
