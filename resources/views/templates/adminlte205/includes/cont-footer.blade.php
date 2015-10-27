9e27820e808502fb7facaa483efbbee3d12a15e9
Werner Maisl <werner@myhomenet.at>
2015-10-27 21:01:32 +0100

    Updated the version number

--- resources/views/templates/adminlte205/includes/cont-footer.blade.php
+++ resources/views/templates/adminlte205/includes/cont-footer.blade.php
@@ -1,4 +1,4 @@
 <div class="pull-right hidden-xs">
-    <b>Version</b> 2.0.0-beta.6
+    <b>Version</b> 2.0.0-rc.1
 </div>
 Copyright &copy; 2013-2015  Store Plugin: <strong>Alon Gubkin</strong>, <strong>Keith Warren (Drixevel)</strong> - &copy; 2015 WebPanel: <strong>Werner Maisl</strong>. All rights reserved.
